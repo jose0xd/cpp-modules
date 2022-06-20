85073833263ffa336500d8766c9ad7b6a703f2b7
Jose francisco Arredondo amador <jarredon@c2r9s4.42malaga.com>
2022-06-20 13:39:05 +0200

    Fix 00/ex01 eof in input

--- 00/ex01/main.cpp
+++ 00/ex01/main.cpp
@@ -6,7 +6,7 @@
 /*   By: jarredon <jarredon@student.42malaga>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/05/24 13:10:55 by jarredon          #+#    #+#             */
-/*   Updated: 2022/06/10 11:23:06 by jarredon         ###   ########.fr       */
+/*   Updated: 2022/06/20 13:38:41 by jarredon         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,6 +14,7 @@
 #include "Contact.hpp"
 #include "PhoneBook.hpp"
 //#include <cstdlib>
+//#include <cstdio>
 
 int	main()
 {
@@ -25,6 +26,12 @@ int	main()
 		std::cout << "\nIntroduce a command (add, search or exit):\n";
 		std::cin >> command;
 		std::cin.ignore(256, '\n');
+		if (std::cin.eof() || std::cin.fail())
+		{
+			std::cin.clear();
+			std::clearerr(stdin); // To clear eof (ctrl+d)
+			command.clear();
+		}
 		if (command == "add" || command == "ADD")
 			book.add_contact();
 		else if (command == "search" || command == "SEARCH")
--- 01/ex06/main.cpp
+++ 01/ex06/main.cpp
@@ -6,7 +6,7 @@
 /*   By: jarredon <jarredon@student.42malaga>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/05/28 08:35:42 by jarredon          #+#    #+#             */
-/*   Updated: 2022/05/28 20:53:04 by jarredon         ###   ########.fr       */
+/*   Updated: 2022/06/17 21:09:25 by jarredon         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,7 +17,7 @@ int main(int ac, char **av)
 {
 	if (ac != 2)
 	{
-		std::cout << "Usage: haltFilter DEBUG|INFO|WARNING|ERROR|RANDOM\n";
+		std::cout << "Usage: haltFilter DEBUG|INFO|WARNING|ERROR\n";
 		return (0);
 	}
 	Harl	boy;
