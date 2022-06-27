9385c603d9dc8024a91494da353d6a58e8bfb32c
Jose francisco Arredondo amador <jarredon@c2r10s3.42malaga.com>
2022-06-28 00:44:55 +0200

    Prepare cpp07 to evaluate

--- 07/ex02/main2.cpp
+++ 07/ex02/main2.cpp
@@ -1,12 +1,12 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   main.cpp                                           :+:      :+:    :+:   */
+/*   main2.cpp                                          :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: jarredon <jarredon@student.42malaga.com>   +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/06/21 16:41:09 by jarredon          #+#    #+#             */
-/*   Updated: 2022/06/21 17:43:54 by jarredon         ###   ########.fr       */
+/*   Updated: 2022/06/28 00:44:14 by jarredon         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,12 +17,17 @@
 
 int	main()
 {
+	Array<int> empty = Array<int>();
+	std::cout << "Default constructor, size: " << empty.size() << "\n";
+
+	std::cout << "Array of 7 int, array[2] = 42:\n";
 	Array<int> array(7);
 	array[2] = 42;
 	for (unsigned int i = 0; i < array.size(); i++)
 		std::cout << array[i] << " ";
 	std::cout << "\n";
 
+	std::cout << "Array of 3 string, tab[1] = \"perita\":\n";
 	Array<std::string> tab(3);
 	tab[1] = "perita";
 	for (unsigned int i = 0; i < tab.size(); i++)
